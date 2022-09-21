f99dd8c0824ff39ffbe96ab3851cdb093c898abe
Marnus van Dyk <eyestrikevandyk@gmail.com>
2022-09-21 18:33:27 +0200

    Polished and finished Dialogue
    Arranged scenes, added transitions

--- Assets/Scripts/SceneSwap.cs
+++ Assets/Scripts/SceneSwap.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneSwap : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public GameObject Dialogue;
+    public void StartDialogue()
+    {
+        Dialogue.SetActive(true);
+    }
+
+    public void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GoToChoose()
     {
-        
+        SceneManager.LoadScene("Race_Choosing");
     }
+
+    // Update is called once per frame
+
 }
