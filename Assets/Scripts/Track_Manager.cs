df7d8e6cfaba8e7d1811f0144b248f9d5455de8a
DuncanSmit01 <88308215+DuncanSmit01@users.noreply.github.com>
2022-09-21 15:01:41 +0200

    CheckPoints Set Active

--- Assets/Scripts/Track_Manager.cs
+++ Assets/Scripts/Track_Manager.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class Track_Manager : MonoBehaviour
 {
+    public Stack<CheckPoints> numbers = new Stack<CheckPoints>();
+    public CheckPoints[] ger = new CheckPoints[9];
+    public bool Pcollision = false;
+   
     // Start is called before the first frame update
-    /* void Start()
+    void Start()
     {
-        
+        CheckpointStart();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
-    private void Awake()
+    public void CheckpointStart()
     {
-        Transform checkpointsTr = transform.Find("Checkpoint");
+        CheckPoints Start = new CheckPoints();
+        
+        numbers.Push(ger[8]);
+        numbers.Push(ger[7]);
+        numbers.Push(ger[6]);
+        numbers.Push(ger[5]);
+        numbers.Push(ger[4]);
+        numbers.Push(ger[3]);
+        numbers.Push(ger[2]);
+        numbers.Push(ger[1]);
+        numbers.Push(ger[0]);
 
-        foreach (Transform checkpointSingleTr in checkpointsTr)
-        {
-            Debug.Log(checkpointSingleTr);
-        }
-    } */
+        Start = numbers.Peek();
+        Start.gameObject.SetActive(true);
+        numbers.Pop();
+        
+    }
 }
