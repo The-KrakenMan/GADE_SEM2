c787e8998925414071e86f13fd183539323be0ac
Marnus van Dyk <eyestrikevandyk@gmail.com>
2022-09-21 15:07:20 +0200

    DIALOGUE WRITTEN
    Need to implement a read

---
+++ Assets/Scripts/JSON_Reader.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSON_Reader : MonoBehaviour
+{
+    public TextAsset jsonFile;
+    // Start is called before the first frame update
+    
+    
+    void Start()
+    {
+        List<string> names = new List<string>();
+
+
+    }
+
+    // Update is called once per frame
+    
+}
